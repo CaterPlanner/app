9bbe2f8d7f4bdd74db6af8dd7870cefd70fc2a83
Laststar020203 <laststar020203@github.com>
2020-05-20 16:45:26 +0900

    init project

---
+++ caterplanner-api/src/test/java/com/downfall/caterplanner/CaterplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.downfall.caterplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaterplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
