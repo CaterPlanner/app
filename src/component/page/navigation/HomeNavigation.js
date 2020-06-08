a500801867648c1164b6fffd91e5463ff2d12090
EmgKw <johnyu1211@gmail.com>
2020-06-09 01:54:16 +0900

    HomeTab carousel

--- src/component/page/navigation/HomeNavigation.js
+++ src/component/page/navigation/HomeNavigation.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack';
 import PlanCardView from '../screen/Home/PlanCardView'
 import PlanView from '../screen/Home/PlanView'
-
+import HomeContr from '../screen/Home/HomeContr'
 
 
 const Stack = createStackNavigator();
@@ -10,8 +10,9 @@ const Stack = createStackNavigator();
 
 const HomeNavigation = () => {
     return(
-          <Stack.Navigator initalRouteName="PlanCardView">
-            <Stack.Screen name="PlanCardView" component={PlanCardView} />
+          <Stack.Navigator initalRouteName="PlanCardView"
+          screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="HomeContr" component = {HomeContr}/>
             <Stack.Screen name="PlanView" component={PlanView} />
         </Stack.Navigator>
     )
--- src/component/page/screen/Home/PlanCardView.js
+++ src/component/page/screen/Home/PlanCardView.js
@@ -1,15 +1,59 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native'
+import {Image, View, Text, Button} from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 const PlanCardView = ({navigation}) => {
   return(
-    <View>
-      <Text>Hello! I am PlanCardView :D</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('PlanView')}
-      />
+    <View style={{}}>
+
+      <Image source={require('../../../../../asset/sample_Image/Sam.png')}
+      style = {{width : '100%', height: 350,
+      borderTopLeftRadius: 20, borderTopRightRadius:20,
+      borderWidth:0.5,
+      borderColor:'black',
+      elevation:10
+      }}></Image>
+
+      <View 
+      style = {{
+        backgroundColor:'white', alignItems: 'center', paddingTop:'10%',
+        width:"100%",
+        height:"20%",
+        borderRightWidth : 0.5,
+        borderLeftWidth : 0.5, 
+        
+      }}>
+      <Text
+      
+      style = {{fontSize:20,}}
+      
+      >Hello! I am PlanCardView :D</Text>
+      </View>
+
+      
+        
+        <View style={{
+         backgroundColor: 'white',
+         width:'100%',
+         height:30,
+         color: 'gray',
+         alignItems:'center',
+         justifyContent:'center',
+         borderBottomWidth:0.5,
+         borderBottomEndRadius: 20, borderBottomStartRadius:20,
+         borderColor:'black',
+         borderRightWidth : 0.5,
+         borderLeftWidth : 0.5, 
+         elevation:10
+         }}>
+      <TouchableOpacity onPress={() => navigation.navigate('PlanView')}>
+           <Text style = {{color:'gray'}}> 자세히 보기 </Text>
+           </TouchableOpacity>
+         </View>
+      
+
+
     </View>
   );
 }
