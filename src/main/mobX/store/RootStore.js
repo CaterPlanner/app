883ce55587524c670cf4006f8dfe9ed20bf7e885
FunCorder <44064404+Laststar020203@users.noreply.github.com>
2020-07-27 09:57:01 +0900

    rest javascript -> java 변경 후 react native 코드 적용

--- src/main/mobX/store/RootStore.js
+++ src/main/mobX/store/RootStore.js
@@ -1,29 +1,8 @@
 import DetailPlanTreeStore from './DetailPlanTreeStore'
-import SQLiteManager from '../../sqlite/SQLiteManager';
 
-import DetailPlanService from '../../rest/service/DetailPlansService';
-import PurposeService from '../../rest/service/PurposeService';
-
-import BriefingRepository from '../../rest/repository/BriefingRepository';
-import DetailPlanHeaderRepository from '../../rest/repository/DetailPlanHeaderRepository';
-import DetailPlanRepository from '../../rest/repository/DetailPlanRepository';
-import PurposeRepository from '../../rest/repository/PurposeRepository';
 
 export default class RootStore {
     constructor(){
         this.detailPlanTreeStore = new DetailPlanTreeStore(this);
-        this.sqliteManager = new SQLiteManager(this);
-
-        //rest
-        this.service = {
-            detailPlanService : new DetailPlanService(this),
-            purposeService : new PurposeService(this)
-        };
-        this.repository = {
-            briefingRepository : new BriefingRepository(this),
-            detailPlanHeaderRepository : new DetailPlanHeaderRepository(this),
-            detailPlanRepository : new DetailPlanRepository(this),
-            purposeRepository : new PurposeRepository(this)
-        }
     }
 }
